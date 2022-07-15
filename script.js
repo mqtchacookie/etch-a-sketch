b571c97ca84bd83ea3c34ed138c1cc30a9e87280
matchacookie <tongkhaiminh@gmail.com>
2022-07-15 13:40:34 +0700

    Add square limit and format button to create grid

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // VAR DECLARATION
 const grid = document.querySelector('.grid');
-const itemAmount = 18;
+const promptButton = document.querySelector('.prompt-button');
+
 // ADD CLASSES
 
 //APPEND DIVS TO GRID
-for (let i = 1; i <= itemAmount ** 2; i++) {
-	const div = document.createElement('div');
-	div.classList.add('grid-item');
-	div.addEventListener('mouseover', () => {
-		div.classList.add('hovered');
-	});
-	grid.appendChild(div);
-}
+const gridCreate = () => {
+	const itemAmount = prompt('enter less than 100');
+	if (itemAmount >= 100) {
+		alert('TOO MANY');
+		return;
+	}
+	for (let i = 1; i <= itemAmount ** 2; i++) {
+		const div = document.createElement('div');
+		div.classList.add('grid-item');
+		div.addEventListener('mouseover', () => {
+			div.classList.add('hovered');
+		});
+		grid.classList.add('grid-style');
+		grid.appendChild(div);
+	}
+};
+
+//BUTTON LISTENER
+promptButton.addEventListener('click', () => {
+	gridCreate();
+});
