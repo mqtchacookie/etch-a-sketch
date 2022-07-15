cb058b156e02188871a56eb7909e0856f3b53508
matchacookie <tongkhaiminh@gmail.com>
2022-07-15 12:38:30 +0700

    Add button

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // VAR DECLARATION
 const grid = document.querySelector('.grid');
-
+const itemAmount = 18;
 // ADD CLASSES
 
 //APPEND DIVS TO GRID
-for (let i = 1; i <= 16 * 16; i++) {
+for (let i = 1; i <= itemAmount ** 2; i++) {
 	const div = document.createElement('div');
 	div.classList.add('grid-item');
-    div.addEventListener('mouseover', () =>{
-        div.classList.add('hovered')
-    })
+	div.addEventListener('mouseover', () => {
+		div.classList.add('hovered');
+	});
 	grid.appendChild(div);
-    
 }
